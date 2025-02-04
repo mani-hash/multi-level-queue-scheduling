41d35b5acd01d812e473c10ce3e3dc140d37467b
mani-hash <manimehalan400@gmail.com>
2025-02-04 22:12:51 +0530

    feat: added process table class

---
+++ include/Process/ProcessTable.h
@@ -0,0 +1,25 @@
+#ifndef PROCESSTABLE_H
+#define PROCESSTABLE_H
+
+#include <vector>
+#include "ProcessControlBlock.h"
+
+namespace Process
+{
+
+    class ProcessTable 
+    {
+        private:
+            std::vector<ProcessControlBlock> processes;
+
+        public:
+            void addProcess(const ProcessControlBlock& process);
+
+            void removeProcess(int processId);
+
+            ProcessControlBlock* getProcess(int processId);
+    };
+
+} // namespace Process
+
+#endif // PROCESSTABLE_H
---
+++ src/Process/ProcessTable.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include "ProcessTable.h"
+
+namespace Process
+{
+
+    void ProcessTable::addProcess(const ProcessControlBlock& process)
+    {
+        processes.push_back(process);
+    }
+
+    void ProcessTable::removeProcess(int processId)
+    {
+        processes.erase(std::remove_if(
+            processes.begin(),
+            processes.end(),
+            [processId](const ProcessControlBlock& process)
+            {
+                return process.getProcessId() == processId;
+            }
+        ));
+    }
+
+    ProcessControlBlock* ProcessTable::getProcess(int processId)
+    {
+        for (auto& process: processes)
+        {
+            if (process.getProcessId() == processId)
+            {
+                return &process;
+            }
+        }
+
+        return nullptr;
+
+    }
+
+} // namespace Process
