a093a8ed5fb04538150846b62b7f8fd3ae2b8483
mani-hash <manimehalan400@gmail.com>
2025-02-21 21:31:32 +0530

    feat: function declarations for process generator

--- include/Process/Generator/ProcessGenerator.h
+++ include/Process/Generator/ProcessGenerator.h
@@ -1,8 +1,10 @@
 #ifndef PROCESSGENERATOR_H
 #define PROCESSGENERATOR_H
 
+#include <memory>
 #include "process.h"
 #include "ProcessControlBlock.h"
+#include "ProcessTable.h"
 #include "RandomNumberGenerator.h"
 
 namespace Process::Generator
@@ -10,19 +12,30 @@ namespace Process::Generator
 
     class ProcessGenerator 
     {
+        private:
+            std::shared_ptr<ProcessTable> processTable;
+
+            static int processIdCounter;
+
+            int getPriorityForProcess();
+
+            int generateProcessId();
+
+            int generateBurstTime();
+
+            ProcessControlBlock createProcess(int priorityLevel);
+
         protected:
             int noOfProcesses = 0;
             PriorityRange priorityRange;
             Utility::RandomNumberGenerator rng;
 
         public:
-            ProcessGenerator(const PriorityRange priorityRange);
+            ProcessGenerator(const PriorityRange priorityRange, std::shared_ptr<ProcessTable> processTable);
 
             virtual void getProcessCountFromUser();
 
-            int getPriorityForProcess();
-
-            ProcessControlBlock* createProcess(int priorityLevel);
+            void generate();
     };
 
 } // namespace Process::Generator
