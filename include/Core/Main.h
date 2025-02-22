70417169aff6f9e208fc74a3c8e28b5df0d19a7e
mani-hash <manimehalan400@gmail.com>
2025-02-22 23:12:49 +0530

    feat: main class header

---
+++ include/Core/Main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <memory>
+#include "Process/ProcessTable.h"
+#include "Scheduler/BaseScheduler.h"
+#include "Scheduler/FirstInFirstOut.h"
+#include "Scheduler/RoundRobin.h"
+#include "Scheduler/ShortestJobFirst.h"
+
+namespace Core
+{
+
+    class Main 
+    {
+        private:
+            std::shared_ptr<Process::ProcessTable> processTable;
+
+            std::unique_ptr<Scheduler::BaseScheduler> queues[4];
+
+        public:
+            Main(std::shared_ptr<Process::ProcessTable> processTable);
+
+            void execute();
+    };
+
+} // namespace Core
+
+#endif // MAIN_H
