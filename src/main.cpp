e5167e8e5df2ee962df95bbacdfa82978e0ff9d5
mani-hash <manimehalan400@gmail.com>
2025-02-23 09:45:05 +0530

    feat (update): entry point to application

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+#include "Core/Init.h"
+#include "Core/Main.h"
+#include "Utility/TimeTracker.h"
+
 int main()
 {
+    Core::Init& Init = Core::Init::getInstance();
+    Utility::TimeTracker& time = Utility::TimeTracker::getInstance();
+    Core::Main main(Init.getProcessTable());
 
+    main.execute();
     return 0;
 }
