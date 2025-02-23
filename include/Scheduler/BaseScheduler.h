827ddd3080ba5af854628edaf8adc9d6e9ca8368
mani-hash <manimehalan400@gmail.com>
2025-02-23 09:42:21 +0530

    feat: parent scheduler class

---
+++ include/Scheduler/BaseScheduler.h
@@ -0,0 +1,28 @@
+#ifndef BASESCHEDULER_H
+#define BASESCHEDULER_H
+
+#include <iostream>
+#include "Process/ProcessControlBlock.h"
+
+namespace Scheduler
+{
+
+    class BaseScheduler 
+    {
+        protected:
+            std::string queueName;
+        public:
+            virtual ~BaseScheduler() = default;
+            
+            virtual void addToQueue(Process::ProcessControlBlock& process) = 0;
+
+            virtual int removeFromQueueIfComplete() = 0;
+
+            virtual void execute() = 0;
+
+            virtual void idleQueue() = 0;
+    };
+
+} // namespace Scheduler
+
+#endif // BASESCHEDULER_H
